16be47d11e9e969415b0f3949ccd52ad329e7429
Johannes Krieger <johannes@kriegers.eu>
2018-07-21 18:24:46 +0200

    Player jump

--- src/httf/blockbounce/game/Game.java
+++ src/httf/blockbounce/game/Game.java
@@ -42,6 +42,10 @@ public class Game extends GameState{
 	
 	private double playerY = 50;
 	
+	private double jumpTime = 0;
+	
+	private double jumpTaste = 100;
+	
 	
 	private AnimationTimer timer = new AnimationTimer() {
 		
@@ -138,6 +142,18 @@ public class Game extends GameState{
 		if(playerY >= 167) 
 		{
 			playerY = 167;
+		}
+		if(jumpTaste > 0) {
+			jumpTaste --;
+		}
+		if(jumpTaste == 0) {
+			jumpTime = 25;
+			jumpTaste = 100;
+		}
+		
+		if(jumpTime > 0) {
+			playerY -=6.5;
+			jumpTime --;
 		}
 	}
 	private void render(double dt) {
